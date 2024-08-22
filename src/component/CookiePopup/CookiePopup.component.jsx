6f199fb454c7035e1c4803fc24eac08678b5f968
xenofon <pantsosxen23@gmail.com>
2024-08-22 16:00:34 +0300

    Initial commit

---
+++ src/component/CookiePopup/CookiePopup.component.jsx
@@ -0,0 +1,12 @@
+import {
+    CookiePopupComponent as SourceCookiePopupComponent,
+} from 'SourceComponent/CookiePopup/CookiePopup.component';
+
+import './CookiePopup.style';
+
+/** @namespace myFirstApp/Component/CookiePopup/Component */
+export class CookiePopupComponent extends SourceCookiePopupComponent {
+    // TODO implement logic
+}
+
+export default CookiePopupComponent;
---
+++ src/component/CookiePopup/CookiePopup.config.js
@@ -0,0 +1,6 @@
+import {
+    COOKIE_POPUP as SOURCE_COOKIE_POPUP,
+} from 'SourceComponent/CookiePopup/CookiePopup.config';
+
+// TODO: implement COOKIE_POPUP
+export const COOKIE_POPUP = SOURCE_COOKIE_POPUP;
